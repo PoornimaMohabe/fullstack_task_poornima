cc50f2f49d8ceb766f5c802f3caa9cc4904d8c30
PoornimaMohabe <mohbepurnima@gmail.com>
2025-05-10 11:03:35 +0530

    showing only redis notes

--- src/routes/fetchTasks.js
+++ src/routes/fetchTasks.js
@@ -10,7 +10,7 @@ taskRouter.get('/', async (req, res) => {
     const redisTasks = cacheData ? JSON.parse(cacheData) : [];
     const mongoTasks = await TaskModel.find({}, { _id: 0, name: 1 });
     const allTasks = [...redisTasks, ...mongoTasks.map(t => t.name)];
-    res.status(200).json({ tasks: allTasks });
+    res.status(200).json({ tasks: redisTasks });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server Error' });
